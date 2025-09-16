1b1833827697dae58064ed3f85631bd1d196edf0
Harshit Sahu <harshit.sahu@webknot.in>
2025-09-16 22:40:05 +0530

    Rename provider to cloud_provider outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "cluster_endpoint" {
   description = "Kubernetes cluster endpoint"
-  value       = local.provider == "aws" ? module.aws_infrastructure[0].cluster_endpoint : module.gcp_infrastructure[0].cluster_endpoint
+  value       = local.cloud_provider == "aws" ? module.aws_infrastructure[0].cluster_endpoint : module.gcp_infrastructure[0].cluster_endpoint
 }
 
 output "cluster_name" {
@@ -13,21 +13,21 @@ output "region" {
   value       = local.region
 }
 
-output "provider" {
-  description = "Cloud provider used"
-  value       = local.provider
+output "cloud_provider" {
+  description = "Cloud cloud_provider used"
+  value       = local.cloud_provider
 }
 
 output "kubeconfig_command" {
   description = "Command to configure kubectl"
-  value = local.provider == "aws" ? 
+  value = local.cloud_provider == "aws" ? 
     "aws eks update-kubeconfig --region ${local.region} --name ${local.cluster_name}" :
     "gcloud container clusters get-credentials ${local.cluster_name} --region ${local.region}"
 }
 
 output "storage_info" {
   description = "Storage configuration details"
-  value = local.provider == "aws" ? {
+  value = local.cloud_provider == "aws" ? {
     efs_id = module.aws_infrastructure[0].efs_id
     storage_class = "efs-sc"
   } : {
@@ -39,7 +39,7 @@ output "storage_info" {
 output "service_endpoints" {
   description = "Service endpoints and access information"
   value = {
-    ingress_controller = local.provider == "aws" ? module.aws_infrastructure[0].ingress_controller_endpoint : module.gcp_infrastructure[0].ingress_controller_endpoint
+    ingress_controller = local.cloud_provider == "aws" ? module.aws_infrastructure[0].ingress_controller_endpoint : module.gcp_infrastructure[0].ingress_controller_endpoint
     services = {
       for service_name, service_config in local.services :
       service_name => {
